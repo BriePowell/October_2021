3e4164a1023c9ca822e8f0f119e077d7d76b9f00
Brie <76833153+BriePowell@users.noreply.github.com>
2021-10-20 02:22:50 -0400

    first commit

---
+++ homework_main.js
@@ -0,0 +1,39 @@
+//==================Exercise #1 ==========//
+
+let arr = [1, 2, 3, 5, 6, 7]
+
+function firstNonCon(arr){
+    for i in arr != i += 1 
+}
+console.log(i)
+//Call function here with arguments
+
+
+
+
+//============Exercise #2 ============//
+
+let addPositive = (input1 = [10, 12, -9, 3, -1, 0, 15]) => {
+    let output = 0
+    for (let i of input1){
+      if (i >= 0){
+        output += i
+      }
+    }
+    return output
+  }
+
+console.log(addPositive());
+
+
+let addPositive = (input2 = [3, 5, 7, 9, -10, 2, -22, -1]) => {
+    let output = 0
+    for (let i of input2){
+      if (i >= 0){
+        output += i
+      }
+    }
+    return output
+  }
+
+console.log(addPositive());
